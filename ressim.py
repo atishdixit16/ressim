59a9708e28fdee958309574aa66fea95bc21b2d9
Atish Dixit <atishdixit16@gmail.com>
2020-07-26 18:46:46 +0100

    avoid division by zero warning

--- ressim.py
+++ ressim.py
@@ -433,6 +433,7 @@ class SaturationEquation(Parameters):
 
         vi = xp[:,0:nx] + yp[0:ny,:] + xn[:,1:nx+1] + yn[1:ny+1,:]
         vi = vi.reshape(grid.ncell)
+        vi[vi==0] = np.finfo(float).eps # to avoid division by zero
         pm = np.min( pv/vi + qp)
         cfl = ((1-s_oir-s_wir)/3)*pm
         Nts = np.ceil(dt/cfl)
---
+++ solver_comparison.py
@@ -0,0 +1,5 @@
+from test_cases import test_case_scipy, test_case_implicit, test_case_explicit
+
+s_explicit, t_explicit = test_case_explicit()
+s_scipy, t_scipy = test_case_scipy()
+s_implicit, t_implicit = test_case_implicit()
